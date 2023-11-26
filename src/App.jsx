d1486e8004006883d7f1f67d34e92a31314a0cd9
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-11-27 00:19:54 +0530

    Some styling added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
-    <div>
+    <div className="flex justify-center items-center  flex-col h-screen w-screen m-0 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
       <div className="text-6xl italic mb-8">Shahid Dhariwala Learning RTL</div>
       <Counter />
     </div>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,18 +4,18 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className="flex border-2 flex-col gap-8 p-2">
+    <div className="flex border-2 flex-col gap-8 p-12 shadow-xl w-max rounded-lg">
       <div className="text-2xl bolder">Count is: {count}</div>
       <div className="flex gap-6 align-center justify-center">
         <button
           onClick={() => setCount((c) => c + 1)}
-          className="bg-orange-500 text-lg p-2 text-white rounded-md hover:bg-orange-700"
+          className="bg-orange-500 text-lg p-2 text-white rounded-md hover:bg-orange-700 shadow-2xl"
         >
           Add +1
         </button>
         <button
           onClick={() => setCount(0)}
-          className="bg-red-500 text-lg p-2 text-white rounded-md hover:bg-red-700"
+          className="bg-red-500 text-lg p-2 text-white rounded-md hover:bg-red-700 shadow-2xl"
         >
           Reset
         </button>
